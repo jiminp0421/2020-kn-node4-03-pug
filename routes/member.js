37baa1e9d943f4063d72f73ac9d0a16027ccbe47
jiminp0421 <jm00476@naver.com>
2020-10-22 22:22:30 +0900

    퍼그를사용해사이트만들기

---
+++ routes/member.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router(); // = new Router();
+
+router.get('/login', (req, res, next) => {
+	const pug = {
+		title: "로그인페이지",
+		scriptFile: '../js/f-member.js'
+	}
+	res.render('./member/login.pug', pug);
+});
+
+router.get('/join', (req, res, next) => {
+	const pug = {
+		title: "로그인페이지",
+		scriptFile: '../js/f-join.js'
+	}
+	res.render('./member/join.pug', pug);
+});
+
+
+module.exports = router;
+
+
+
+//https://html2pug.now.sh/ 퍼그로 코드 바꾼후 컨버팅시키기 코드다지움
